0d6780850988dfac6b71398a3c47228ab16c47a9
Dovt999 <121645824+Dovt999@users.noreply.github.com>
2023-01-29 16:37:28 +0300

    added HW6

---
+++ HW6/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("How many numbers are you going to insert?");
+int numamount = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[numamount];
+Console.WriteLine("Insert the numbers");
+int amount = 0;
+for(int i=0;i<numamount;i++)
+{
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+    if(nums[i]>0) amount++;
+}
+Console.WriteLine("The amount of numbers greater than 0 is " + amount.ToString());
---
+++ HW6/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// y = k1 * x + b1, y = k2 * x + b2
+// k1 * x + b1 = k2 * x + b2
+// k1 * x - k2 * x = b2 - b1
+// x = (b2 - b1)/(k1-k2)
+// y = k1 * x + b1
+// (x; y)
+
+void intersection(double k1, double k2, double b1, double b2)
+{
+    Console.WriteLine("(" + ((b2 - b1) / (k1 - k2)).ToString() + "; " + (((b2 - b1) / (k1 - k2)) * k1 + b1).ToString() + ")");
+}
+Console.WriteLine("Insert k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+intersection(k1, k2, b1, b2);
